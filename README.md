e9854b50d31ae269a9faca1c1bd669090571352f
Hsiao-Ting <sst.dreams@gmail.com>
2022-07-31 00:34:42 +0800

    feat: refine timezone and add time checks

--- README.md
+++ README.md
@@ -21,11 +21,12 @@ to a folder named `imgs`. Then running the following command to process and uplo
 
 ```bash
 mapillary_tools process imgs --interpolate_directions
-mapillary_cleanup -h "-8" -m 1 imgs
+mapillary_cleanup -z "Asia/Taipei" -m 1 imgs
 mapillary_tools upload imgs
 ```
 
 Args:
-* `-h`: hours to be added or substracted (default: 0).
+* `-z`: Time zone recognized by tzinfo, used to convert timestamp to UTC (default: UTC).
 * `-m`: distance in meters to be considered as duplicate (default: 0, which will not apply checks).
-* `-n`: Images to be included in the sequences. (default: 150)
+* `-n`: Maximum images (including duplicates) in a sequence. (default: 150)
+* `-s`: Seconds between images to split sequences. (default: 30)
--- mapillary_cleanup/main.py
+++ mapillary_cleanup/main.py
@@ -3,6 +3,7 @@ from json import load, dump
 from pathlib import Path
 from typing import IO, Any, Optional
 from uuid import uuid4
+from zoneinfo import ZoneInfo
 
 from click import argument, command, option, Path as ClickPath
 from geopy.distance import geodesic
@@ -12,33 +13,51 @@ FORMAT = "%Y_%m_%d_%H_%M_%S_%f"
 
 
 @command()
-@option("-h", default=0, type=float, help="Hours to be added / substracted")
+@option(
+    "-z",
+    default="UTC",
+    type=str,
+    help="Time zone recognized by tzinfo, used to convert timestamp to UTC",
+)
 @option("-m", default=0, type=float, help="Meters to be considered as duplicate point")
-@option("-n", default=150, type=int, help="Images to split a sequence")
+@option(
+    "-n",
+    default=150,
+    type=int,
+    help="Maximum images (including duplicates) in a sequence",
+)
+@option("-s", default=30, type=int, help="Seconds between images to split sequences")
 @argument(
     "input",
     type=ClickPath(exists=True, file_okay=False, writable=False, resolve_path=True),
 )
-def process(h: float, m: float, n: int, input: IO):
+def process(z: str, m: float, n: int, s: int, input: IO):
     got = 0
     skipped = 0
     prev_point: Optional[tuple[float, float]] = None
+    prev_time: Optional[datetime] = None
     json_path: Path = Path(input) / "mapillary_image_description.json"
     errors: list[dict[str, Any]] = []
     results: list[dict[str, Any]] = []
     seq_id = str(uuid4())
+
+    zone = ZoneInfo(z)
     with json_path.open("r+") as fd:
         orig = load(fd)
         for img in orig:
+            time = datetime.strptime(img["MAPCaptureTime"], FORMAT).replace(tzinfo=zone)
             if "error" in img:
                 errors.append(img)
                 continue
             point: tuple[float, float] = img["MAPLatitude"], img["MAPLongitude"]
-            if got == n:
+            if got == n or (
+                prev_time is not None and time - prev_time >= timedelta(seconds=s)
+            ):
                 seq_id = str(uuid4())
                 got = 1
             else:
                 got += 1
+            prev_time = time
             if m > 0:
                 dist = (
                     geodesic(prev_point, point).m
@@ -59,12 +78,10 @@ def process(h: float, m: float, n: int, input: IO):
                     skipped += 1
                     continue
 
-            if h != 0:
-                time = datetime.strptime(img["MAPCaptureTime"], FORMAT)
-                img["MAPCaptureTime"] = datetime.strftime(
-                    time + timedelta(hours=h), FORMAT
-                )[:-3]
-            
+            img["MAPCaptureTime"] = datetime.strftime(
+                time.astimezone(ZoneInfo("UTC")), FORMAT
+            )[:-3]
+
             img["MAPSequenceUUID"] = seq_id
             results.append(img)
 
